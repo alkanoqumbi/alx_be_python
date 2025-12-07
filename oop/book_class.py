9548669c861fbd1820801a4d0fb7a4448704ba6e
halkanoqumbi <halkanoqumbi@gmail.com>
2025-12-07 20:52:16 +0300

    fix:corection

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,17 @@
 class Book:
-    def __init__(self, title, author, year):
+    def _init_(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
 
-    def __str__(self):
+    def _str_(self):
+        
         return f"{self.title} by {self.author}, published in {self.year}"
 
-    def __repr__(self):
+    def _repr_(self):
+        
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
-    def __del__(self):
+    def _del_(self):
+        
         print(f"Deleting {self.title}")
--- oop/main.py
+++ oop/main.py
@@ -1,12 +1,17 @@
 from book_class import Book
 
 def main():
+    
     my_book = Book("1984", "George Orwell", 1949)
 
+    
     print(my_book)
+
+    
     print(repr(my_book))
 
+    
     del my_book
 
-if __name__ == "__main__":
+if __name__ == "_main_":
     main()
